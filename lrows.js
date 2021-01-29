a1955dc9e9303bbd7b999e38accf9c9dbea22f1c
ArqenDesign <76055626+ArqenDesign@users.noreply.github.com>
2021-01-29 16:41:14 +0300

    Add files via upload

---
+++ lrows.js
@@ -0,0 +1,192 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const moment = require("moment");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const db = require("croxydb");
+const http = require("http");
+const express = require("express");
+require("./util/eventLoader.js")(client);
+const path = require("path");
+const snekfetch = require("snekfetch");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`Lrows V12 Guard Aktif`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   lrowsconsole.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });lrows
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+client.login(ayarlar.token);
+
+//------------------------KANAL KORUMA-----------------------------\\
+
+client.on("channelDelete", async channel => {
+  const entry = await channel.guild
+    .fetchAuditLogs({ type: "CHANNEL_DELETE" })
+    .then(audit => audit.entries.first());
+  if (entry.executor.id === client.user.id) return;
+  //  if (entry.executor.id === channel.guild.owner.id) return
+  if (ayarlar.korumakanal) {
+    const embed = new Discord.MessageEmbed();
+    embed.setTitle("Bir Kanal Silindi!");
+    embed.addField("Kanalı Silen", "> `" + entry.executor.tag + "`");
+    embed.addField("Kanalı Silen İD", "> `" + entry.executor.id + "`");
+    embed.addField("Silinen Kanal", "> `" + channel.name + "`");
+    embed.addField("Sonuç;", "Kanal Tekrar Açıldı");
+    embed.setThumbnail(entry.executor.avatarURL());
+    embed.setFooter(channel.guild.name, channel.guild.iconURL());
+    embed.setColor("RED");
+    embed.setTimestamp();
+    client.channels.cache
+      .get(ayarlar.korumakanal)
+      .send(embed)
+      .then(channel.clone().then(x => x.setPosition(channel.position)));
+  }
+});
+
+//---------------------------ROL KORUMA------------------------------\\
+
+client.on("roleDelete", async role => {
+  const entry = await role.guild
+    .fetchAuditLogs({ type: "ROLE_DELETE" })
+    .then(audit => audit.entries.first());
+  if (entry.executor.id === client.user.id) return;
+  // if (entry.executor.id === role.guild.owner.id) return
+  if (ayarlar.korumakanal) {
+    const embed = new Discord.MessageEmbed();
+    embed.setTitle("Bir Rol Silindi!");
+    embed.addField("Rolü Silen", "> `" + entry.executor.tag + "`");
+    embed.addField("Rolü Silen İD", "> `" + entry.executor.id + "`");
+    embed.addField("Silinen Rol", "> `" + role.name + "`");
+    embed.addField("Sonuç;", "Rol Tekrar Açıldı");
+    embed.setThumbnail(entry.executor.avatarURL());
+    embed.setFooter(role.guild.name, role.guild.iconURL());
+    embed.setColor("RED");
+    embed.setTimestamp();
+    client.channels.cache
+      .get(ayarlar.korumakanal)
+      .send(embed)
+      .then(
+        role.guild.roles.create({
+          data: {
+            name: role.name,
+            color: role.color,
+            hoist: role.hoist,
+            permissions: role.permissions,
+            mentionable: role.mentionable,
+            position: role.position
+          },
+          reason: "Silinen Rol Açıldı."
+        })
+      );
+  }
+});
+
+//-----------------------LOG------------------------\\
+
+client.on("ready", () => {
+  client.channels.cache.get("800695864728485929").join();
+});
